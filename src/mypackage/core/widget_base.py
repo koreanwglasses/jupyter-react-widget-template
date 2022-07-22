967ffacbf4c72a937cd6a13c8ad096aaf6d426ff
koreanwglasses <koreanwglasses@gmail.com>
2022-07-21 19:13:47 -0500

    Improved code

--- src/mypackage/core/widget_base.py
+++ src/mypackage/core/widget_base.py
@@ -6,7 +6,38 @@ class WidgetBase:
     def __init__(self, component_name: str, props={}, callables={}):
         self.__component = load_component(component_name)
         self.__props = props
-        self.__callables = dict(callables)
+        self.__event_handlers = {}
+
+        for key, value in dict(callables).items():
+            self.__register_callable(key, value)
+
+        self.__setters = []
+
+    # Define helper functions handling events from component
+    # and returning messages to component
+    def __exec_func(self, func, id, args):
+        try:
+            value = func(*args)
+            error = None
+        except Exception as e:
+            value = None
+            error = e.args
+
+        self.send_message(f"return_value:{id}", value, error)
+
+    def __register_callable(self, name, func):
+        self.__event_handlers["exec_func:" + name] = idom.core.events.EventHandler(
+            lambda data: self.__exec_func(func, data[0], data[1]),
+            target=f"exec_func:{name}",
+        )
+
+    def send_message(self, type, *payload):
+        """
+        TODO
+        """
+
+        for func in self.__setters:
+            func({"type": type, "payload": payload})
 
     @idom.component
     def show(self):
@@ -14,34 +45,10 @@ class WidgetBase:
         # messages to props. This is translated into
         # events on the front-end (js) side in widget-wrapper.tsx
         last_message, set_last_message = idom.hooks.use_state(None)
-
-        # Define helper functions handling events from component
-        # and returning messages to component
-
-        def exec_func(func, id, args):
-            try:
-                value = func(*args)
-                error = None
-            except Exception as e:
-                value = None
-                error = e.args
-
-            set_last_message(
-                {
-                    "type": f"return_value:{id}",
-                    "payload": [value, error],
-                }
-            )
-
-        # Register event handlers
-        event_handlers = {}
-        for key, value in self.__callables.items():
-            event_handlers["exec_func:" + key] = idom.core.events.EventHandler(
-                lambda data: exec_func(value, data[0], data[1]), target=f"exec_func:{key}"
-            )
+        self.__setters.append(set_last_message)
 
         # Create component
         return self.__component(
             {"lastMessage": last_message, "componentProps": self.__props},
-            event_handlers=event_handlers,
+            event_handlers=self.__event_handlers,
         )
